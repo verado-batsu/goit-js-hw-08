e37c947ed08f07c5f75c1d94bab86137da669498
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-01-15 19:54:14 +0200

    fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,16 @@ import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 import { save, load } from "./storage";
 
+
+
 const iframeEl = document.getElementById('vimeo-player');
 
 const player = new Vimeo(iframeEl);
 
 const CURRENT_TIME_KEY = "videoplayer-current-time";
+
+const loadedTime = load(CURRENT_TIME_KEY) || 0;
+
 
 player.on('timeupdate',  throttle(onTimeUpdate, 1000));
 
@@ -14,4 +19,4 @@ function onTimeUpdate({ seconds }) {
 	save(CURRENT_TIME_KEY, seconds);
 }
 
-player.setCurrentTime(load(CURRENT_TIME_KEY));
+player.setCurrentTime(loadedTime);
